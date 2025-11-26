38511c718cb2ad8041348aa35f82cac05054d43b
madelein-milagros <ramoschamorromilagros@gmail.com>
2025-11-25 23:11:28 -0500

    Remove comment from AppNavigation call

--- app/src/main/java/com/tecsup/authfirebaseapp/MainActivity.kt
+++ app/src/main/java/com/tecsup/authfirebaseapp/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AuthFirebaseAppTheme {
-                AppNavigation()   //  ← sin parámetros
+                AppNavigation()   
             }
         }
     }
